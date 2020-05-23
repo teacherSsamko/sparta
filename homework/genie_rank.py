19bd2e2d296600e70ec13333793d34ce1515a632
teacherSsamko <37929557+teacherSsamko@users.noreply.github.com>
2020-05-23 17:09:49 +0900

    3rd homework

---
+++ homework/genie_rank.py
@@ -0,0 +1,26 @@
+import requests
+
+from bs4 import BeautifulSoup
+
+# URL을 읽어서 HTML를 받아오고,
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&ymd=20200403&hh=23&rtm=N&pg=1',headers=headers)
+
+# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+soup = BeautifulSoup(data.text, 'html.parser')
+
+# select를 이용해서, tr들을 불러오기
+songs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+#body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info
+
+# movies (tr들) 의 반복문을 돌리기
+for song in songs:
+    # movie 안에 a 가 있으면,
+    a_tag = song.select_one('td.info > a.title')
+    #body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.title.ellipsis
+    if a_tag is not None:
+        rank = song.select_one('td.number').text[:2].rstrip()
+        #body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.number
+        title = a_tag.text.strip()                                      # a 태그 사이의 텍스트를 가져오기
+        singer = song.select_one('td.info > a.artist').text
+        print(rank,title,singer)
